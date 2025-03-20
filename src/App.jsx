d0e7ab1c617a7081505de4bcc79386e8754318b1
Mustafa Keser <mustafakeser0@hotmail.com>
2025-03-20 18:00:13 +0300

    login completed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./components/login";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import Success from "./components/success";
+
+function App() {
+  return (
+    <div className="app">
+      <Switch>
+        <Route exact path="/">
+          <p>burda olacak logiiin</p>
+          <Login />
+        </Route>
+        <Route path="/home">
+          <Success />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Success.jsx
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div>
+      <h1>WelCome</h1>
+      <p>Burası Anasayfa</p>
+    </div>
+  );
+}
